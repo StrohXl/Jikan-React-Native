822ce1f96f7dfcea9b64efef81816db39395f006
StrohXl <xaviermayora20@gmail.com>
2026-06-10 19:43:25 -0400

    fix: position bottomSheet and fetch filter

--- features/search/FlatListAnimeCard.tsx
+++ features/search/FlatListAnimeCard.tsx
@@ -7,21 +7,22 @@ import Animated, { FadeIn } from "react-native-reanimated";
 import { useContextSearch } from "./hooks/contextSearch";
 import ListEmptyComponent from "./ListEmpty";
 
+const paddingHorizontal = 10;
+const columnGap = 15;
+
 const FlatListAnimeCard = ({
   refFlatList,
 }: {
   refFlatList: React.RefObject<FlatList | null>;
 }) => {
   const { width } = useWindowDimensions();
-  const paddingHorizontal = 10;
-  const columnGap = 15;
+
   const background = useThemeColor({}, "background");
 
-  const { filters, fetchData, textSearch, loadingData, error, data, genres } =
+  const { filters, textSearch, loadingData, error, data, genres } =
     useContextSearch();
 
   useEffect(() => {
-    fetchData();
     refFlatList.current?.scrollToOffset({
       offset: 0,
       animated: false,
@@ -42,6 +43,9 @@ const FlatListAnimeCard = ({
       contentContainerStyle={{
         gap: 15,
         paddingTop: 10,
+        maxWidth: 1200,
+        width: "100%",
+        marginInline: "auto",
         paddingBottom: 20,
       }}
       columnWrapperStyle={{
